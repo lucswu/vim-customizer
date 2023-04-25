fd37d3716134158f9d9df79645f3511894624a74
Lucas Wu <lucaswu@wharton.upenn.edu>
2023-04-24 22:33:47 -0400

    Nearly done with tab_settings, just need to add checks for default tab settings

--- vim-customizer.sh
+++ vim-customizer.sh
@@ -5,14 +5,74 @@ function color_scheme() {
 }
 
 function tab_settings() {
+    # Introduction
     printf "\nWelcome to the tab settings editor. Here, you can edit your tab sizing, whether or not you use the tab character, and your shiftwidth."
-    # tabstop=$()
-    # shiftwidth=$()
-    expandtab=$(grep -E "set expandtab" .vimrc | wc -l)
+
+    # Read in information
+    ts=$(grep -E "^set tabstop=" .vimrc | wc -l)
+    tabstop=8
+    if [ $ts != 0 ]; then
+        tabstop=$(grep -E "^set tabstop=" .vimrc | sed -E "s/^set tabstop=//")
+    fi
+    sw=$(grep -E "^set shiftwidth=" .vimrc | wc -l)
+    shiftwidth=8
+    if [ $sw != 0 ]; then
+        shiftwidth=$(grep -E "^set shiftwidth=" .vimrc | sed -E "s/^set shiftwidth=//")
+    fi
+    expandtab=$(grep -E "^set expandtab$" .vimrc | wc -l)
+
+    # Print current settings
     printf "\nHere are your current settings:\n"
-    printf "\tTabstop: $tabstop\n"
-    printf "\tShiftwidth: $shiftwidth\n"
-    printf "\tExpandtab"
+    printf "\t1) Tabstop: $tabstop\n"
+    printf "\t2) Shiftwidth: $shiftwidth\n"
+    if [ $expandtab == 0 ]; then
+        printf "\t3) Expandtab: Off"
+    else
+        printf "\t3) Expandtab: On"
+    fi
+
+    printf "\nWhat settings would you like to edit? Please type the number here (hit enter to exit): "
+    read option
+    while [ ! -z "$option" ]; do
+        case "$option" in
+        1)
+            printf "\nPlease enter your desired tabstop: "
+            read val
+            sed -E -i "s/^set tabstop=$tabstop$/set tabstop=$val/" .vimrc
+            printf "Tabstop changed to $val!\n"
+            ;;
+        2)
+            printf "\nPlease enter your desired shiftwidth: "
+            read val
+            sed -E -i "s/^set tabstop=$shiftwidth$/set tabstop=$val/" .vimrc
+            printf "Shiftwidth changed to $val!\n"
+            ;;
+        3)
+            printf "\nWould you like Expandtab on? (Y/N): "
+            read opt
+            case "$opt" in
+            Y)
+                if [ $expandtab == 0 ]; then
+                    echo "set expandtab" >>.vimrc
+                fi
+                printf "\nExpandtab is now on."
+                ;;
+            N)
+                sed -E -i "s/^set expandtab$//" .vimrc
+                printf "\nExpandtab is now off."
+                ;;
+            *)
+                printf "\nInvalid option. Please type either \"Y\" or \"N\".\n"
+                ;;
+            esac
+            ;;
+        *)
+            printf "\nInvalid option. Please choose a number 1-3!\n"
+            ;;
+        esac
+        printf "\nWhat settings would you like to edit? Please type the number here (hit enter to exit): "
+        read option
+    done
 }
 
 function general_settings() {
@@ -23,7 +83,7 @@ function general_settings() {
 # Standard Menu
 function menu() {
     printf "\nWelcome to the LP vim customizer! Here, we've set up easy ways to change your vim settings, especially for beginners! What would you like to do?\n"
-    printf "\t[1] Edit Color Scheme\n\t[2] Edit tab settings\n\t[3] General Settings (line numbers, syntax, etc.)\n\t[<Enter> or CTRL+D] to Quit\n"
+    printf "\t[1] Edit Color Scheme\n\t[2] Edit Tab Settings\n\t[3] General Settings (line numbers, syntax, etc.)\n\t[<Enter> or CTRL+D] to Quit\n"
     read option
     while [ ! -z "$option" ]; do
         case "$option" in
@@ -41,7 +101,7 @@ function menu() {
             ;;
         esac
         printf "\nWelcome to the LP vim customizer! Here, we've set up easy ways to change your vim settings, especially for beginners! What would you like to do?\n"
-        printf "\t[1] Edit Color Scheme\n\t[2] Edit tab settings\n\t[3] General Settings (line numbers, syntax, etc.)\n\t[<Enter> or CTRL+D] to Quit\n"
+        printf "\t[1] Edit Color Scheme\n\t[2] Edit Tab Settings\n\t[3] General Settings (line numbers, syntax, etc.)\n\t[<Enter> or CTRL+D] to Quit\n"
         read option
     done
     printf "Thanks for using the LP vim customizer!\n"
