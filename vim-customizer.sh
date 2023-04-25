b405bb17eddd579c591810f2a7e5d0ddd94544a3
Lucas Wu <lucaswu@wharton.upenn.edu>
2023-04-24 22:53:58 -0400

    lucas done with tab settings

--- vim-customizer.sh
+++ vim-customizer.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function color_scheme() {
-    printf "WIP color_scheme!"
-}
-
 function tab_settings() {
     # Introduction
     printf "\nWelcome to the tab settings editor. Here, you can edit your tab sizing, whether or not you use the tab character, and your shiftwidth."
@@ -33,18 +29,28 @@ function tab_settings() {
 
     printf "\nWhat settings would you like to edit? Please type the number here (hit enter to exit): "
     read option
-    while [ ! -z "$option" ]; do
+    if [ ! -z "$option" ]; then
         case "$option" in
         1)
             printf "\nPlease enter your desired tabstop: "
             read val
-            sed -E -i "s/^set tabstop=$tabstop$/set tabstop=$val/" .vimrc
+            if [ $ts != 0 ]; then
+                sed -E -i "s/^set tabstop=$tabstop$/set tabstop=$val/" .vimrc
+            else
+                echo "" >>.vimrc
+                echo "set tabstop=$val" >>.vimrc
+            fi
             printf "Tabstop changed to $val!\n"
             ;;
         2)
             printf "\nPlease enter your desired shiftwidth: "
             read val
-            sed -E -i "s/^set tabstop=$shiftwidth$/set tabstop=$val/" .vimrc
+            if [ $sw != 0 ]; then
+                sed -E -i "s/^set shiftwidth=$shiftwidth$/set shiftwidth=$val/" .vimrc
+            else
+                echo "" >>.vimrc
+                echo "set shiftwidth=$val" >>.vimrc
+            fi
             printf "Shiftwidth changed to $val!\n"
             ;;
         3)
@@ -53,6 +59,7 @@ function tab_settings() {
             case "$opt" in
             Y)
                 if [ $expandtab == 0 ]; then
+                    echo "" >>.vimrc
                     echo "set expandtab" >>.vimrc
                 fi
                 printf "\nExpandtab is now on."
@@ -70,99 +77,91 @@ function tab_settings() {
             printf "\nInvalid option. Please choose a number 1-3!\n"
             ;;
         esac
-        printf "\nWhat settings would you like to edit? Please type the number here (hit enter to exit): "
-        read option
-    done
+    fi
 }
 
-
 function general_settings() {
-  printf "Welcome to the General Settings menu!\n"
+    printf "Welcome to the General Settings menu!\n"
 
-  # List all the possible settings that the user can modify
-  printf "Please select a setting to modify:\n"
-  printf "\t[1] Enable syntax highlighting\n"
-  printf "\t[2] Enable line numbers\n"
-  printf "\t[3] Enable cursor line\n"
-  printf "\t[4] Enable cursor column\n"
-  printf "\t[5] Disable line wrapping\n"
-  printf "\t[<Enter> or CTRL+D] to Quit\n"
-
-  # Read the user's choice
-  read choice
-
-  # Process the user's choice
-  while [ ! -z "$choice" ]; do
-    case "$choice" in
-      1)
-        # Enable syntax highlighting
-        printf "Enabling syntax highlighting...\n"
-        echo "syntax on" >> ~/.vimrc
-        ;;
-      2)
-        # Enable line numbers
-        printf "Enabling line numbers...\n"
-        echo "set number" >> ~/.vimrc
-        ;;
-      3)
-        # Enable cursor line
-        printf "Enabling cursor line...\n"
-        echo "set cursorline" >> ~/.vimrc
-        ;;
-      4)
-        # Enable cursor column
-        printf "Enabling cursor column...\n"
-        echo "set cursorcolumn" >> ~/.vimrc
-        ;;
-      5)
-        # Disable line wrapping
-        printf "Disabling line wrapping...\n"
-        echo "set nowrap" >> ~/.vimrc
-        ;;
-      *)
-        # Invalid option
-        printf "Invalid Option.\n"
-        ;;
-    esac
-
-    # Prompt the user for the next choice
-    printf "\nPlease select a setting to modify:\n"
+    # List all the possible settings that the user can modify
+    printf "Please select a setting to modify:\n"
     printf "\t[1] Enable syntax highlighting\n"
     printf "\t[2] Enable line numbers\n"
     printf "\t[3] Enable cursor line\n"
     printf "\t[4] Enable cursor column\n"
     printf "\t[5] Disable line wrapping\n"
     printf "\t[<Enter> or CTRL+D] to Quit\n"
+
+    # Read the user's choice
     read choice
-  done
 
-  printf "\nFinished modifying General Settings.\n"
+    # Process the user's choice
+    while [ ! -z "$choice" ]; do
+        case "$choice" in
+        1)
+            # Enable syntax highlighting
+            printf "Enabling syntax highlighting...\n"
+            echo "syntax on" >>~/.vimrc
+            ;;
+        2)
+            # Enable line numbers
+            printf "Enabling line numbers...\n"
+            echo "set number" >>~/.vimrc
+            ;;
+        3)
+            # Enable cursor line
+            printf "Enabling cursor line...\n"
+            echo "set cursorline" >>~/.vimrc
+            ;;
+        4)
+            # Enable cursor column
+            printf "Enabling cursor column...\n"
+            echo "set cursorcolumn" >>~/.vimrc
+            ;;
+        5)
+            # Disable line wrapping
+            printf "Disabling line wrapping...\n"
+            echo "set nowrap" >>~/.vimrc
+            ;;
+        *)
+            # Invalid option
+            printf "Invalid Option.\n"
+            ;;
+        esac
+
+        # Prompt the user for the next choice
+        printf "\nPlease select a setting to modify:\n"
+        printf "\t[1] Enable syntax highlighting\n"
+        printf "\t[2] Enable line numbers\n"
+        printf "\t[3] Enable cursor line\n"
+        printf "\t[4] Enable cursor column\n"
+        printf "\t[5] Disable line wrapping\n"
+        printf "\t[<Enter> or CTRL+D] to Quit\n"
+        read choice
+    done
+
+    printf "\nFinished modifying General Settings.\n"
 }
-
-
 
 # Standard Menu
 function menu() {
     printf "\nWelcome to the LP vim customizer! Here, we've set up easy ways to change your vim settings, especially for beginners! What would you like to do?\n"
-    printf "\t[1] Edit Color Scheme\n\t[2] Edit Tab Settings\n\t[3] General Settings (line numbers, syntax, etc.)\n\t[<Enter> or CTRL+D] to Quit\n"
+    printf "\t[1] Edit General Settings\n\t[2] Edit Tab Settings\n\t[<Enter> or CTRL+D] to Quit\n"
     read option
     while [ ! -z "$option" ]; do
         case "$option" in
         1)
-            color_scheme
+            general_settings
             ;;
         2)
             tab_settings
-            ;;
-        3)
-            general_settings
             ;;
         *)
             printf "Invalid Option.\n"
             ;;
         esac
         printf "\nWelcome to the LP vim customizer! Here, we've set up easy ways to change your vim settings, especially for beginners! What would you like to do?\n"
-        printf "\t[1] Edit Color Scheme\n\t[2] Edit Tab Settings\n\t[3] General Settings (line numbers, syntax, etc.)\n\t[<Enter> or CTRL+D] to Quit\n"
+        printf "\t[1] Edit General Settings\n\t[2] Edit Tab Settings\n\t[<Enter> or CTRL+D] to Quit\n"
         read option
     done
     printf "Thanks for using the LP vim customizer!\n"
